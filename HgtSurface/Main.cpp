1cd88b0734e1a85584af2f3e1548f72651479e58
David Thompson <daviddt17@gmail.com>
2015-12-04 18:18:10 -0500

    OpenGl setup stuff

--- HgtSurface/Main.cpp
+++ HgtSurface/Main.cpp
@@ -8,22 +8,105 @@
 
 using namespace std;
 
-int main()
+// Macros
+#define HGT_ROW_SIZE 1201
+
+// Globals
+GLuint* vao;
+GLuint* vbo;
+GLint modelLocation, viewLocation, projectionLocation, colorLocation;
+int heightData[HGT_ROW_SIZE * 2];
+float scaledHeightData[HGT_ROW_SIZE * 2];
+
+void loadHgtData(const char* fileName)
 {
-	int wait;
-	int heightBuffer[ONE_ARC_SIZE * 2];
-	float scaleBuffer[ONE_ARC_SIZE * 2];
+	// Loads height data
+	Hgt::hgtLoader(fileName, heightData, Hgt::ARC_TYPES::ONE_ARC);
 
-	Hgt::hgtLoader("N36W112.hgt", heightBuffer, Hgt::ARC_TYPES::ONE_ARC);
+	// Scales data between -1 and 1
+	Hgt::scaleHgtBuffer(heightData, Hgt::ARC_TYPES::ONE_ARC, scaledHeightData, 1);
+}
 
-	Hgt::scaleHgtBuffer(heightBuffer, Hgt::ARC_TYPES::ONE_ARC, scaleBuffer, 1);
+void init()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 
-	for (int i = 0; i < ONE_ARC_SIZE * 2; ++i)
+	glLineWidth(2.0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	glGenBuffers(1, vbo);
+	glGenVertexArrays(1, vao);
+
+	// Load Shaders
+	ShaderInfo shaders[] =
 	{
-		cout << "Height " << i << ": " << heightBuffer[i] << "\n";
-		cout << "Scaled " << i << ": " << scaleBuffer[i] << "\n";
-	}
+		{ GL_VERTEX_SHADER, "vertexShader.vert" },
+		{ GL_FRAGMENT_SHADER, "fragmentShader.frag" },
+		{ GL_NONE, NULL }
+	};
 
-	cin >> wait;
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(program);
+
+	// Plane
+	glBindVertexArray(vao[0]);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(scaledHeightData), scaledHeightData, GL_STATIC_DRAW);
+
+	GLuint position = glGetAttribLocation(program, "position");
+	glEnableVertexAttribArray(position);
+	glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	
+	// Load uniform variable location
+	viewLocation = glGetUniformLocation(program, "viewTransform");
+	modelLocation = glGetUniformLocation(program, "modelTransform");
+	projectionLocation = glGetUniformLocation(program, "projectionTransform");
+	colorLocation = glGetUniformLocation(program, "color");
+
+	glBindVertexArray(0);
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+void timer(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(1000 / 30, timer, 1);
+}
+
+void keypress(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+		default:
+			break;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// Load HGT
+	loadHgtData("N36W112.hgt");
+	glutInit(&argc, argv);
+
+	// Presets
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("Main Window");
+	glutInitContextVersion(3, 1);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glewInit();
+
+	// Bind functions
+	glutKeyboardFunc(keypress);
+	glutDisplayFunc(display);
+	glutTimerFunc(1000 / 30, timer, 1);
+
+	glutMainLoop();
     return 0;
 }
